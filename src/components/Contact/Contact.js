3dfa6e35bc2f4bcf84ea7a1219a9fc8924369799
frugarcia <frugarcia@gmail.com>
2019-04-15 18:13:03 +0200

    adding contact form and icon github in footer

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -12,8 +12,26 @@ const Contact = () => {
         Escríbeme a:{" "}
         <strong>
           <a href="mailto:fernandoruiz@frudev.com">fernandoruiz@frudev.com</a>
-        </strong>
+        </strong>{" "}
+        o rellene el siguiente formulario:
       </span>
+      <div className="contact-form">
+        <h3>Formulario de contacto:</h3>
+        <form method="POST" action="https://formspree.io/frugarcia@gmail.com">
+          <label>Tu nombre y apellidos / Empresa:</label>
+          <input type="text" required name="name" placeholder="Tu nombre..." />
+          <label>Email:</label>
+          <input type="email" required name="email" placeholder="Tu email..." />
+          <label>Mensaje:</label>
+          <textarea
+            name="message"
+            required
+            placeholder="Escriba el mensaje..."
+            rows="5"
+          />
+          <button type="submit">Enviar formulario</button>
+        </form>
+      </div>
     </section>
   );
 };
